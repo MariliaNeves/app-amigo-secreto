639b42299a69d61a9b2ac07c6fda7f4d5d60ece5
Marília Neves <marilia.neves@neainformatica.local>
2019-11-21 08:47:58 -0400

    add project

---
+++ AmigoSecreto/app/src/main/java/com/example/mcandi/amigosecreto/Amigo.java
@@ -0,0 +1,38 @@
+package com.example.mcandi.amigosecreto;
+
+/**
+ * Created by mcandi on 09/11/2017.
+ */
+
+public class Amigo {
+
+  public String nomeParticipante;
+  public String telefone;
+  public  String seuAmigoSecreto;
+
+  public String getSeuAmigoSecreto() {
+    return seuAmigoSecreto;
+  }
+
+  public void setSeuAmigoSecreto(String seuAmigoSecreto) {
+    this.seuAmigoSecreto = seuAmigoSecreto;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+
+  public String getNomeParticipante() {
+    return nomeParticipante;
+  }
+
+  public void setNomeParticipante(String nomeParticipante) {
+    this.nomeParticipante = nomeParticipante;
+  }
+
+
+}
